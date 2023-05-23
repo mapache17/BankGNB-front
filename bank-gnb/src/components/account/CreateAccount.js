ea2f411d13df921727f7918c67725c088a85f353
mapache17 <mota.170903@gmail.com>
2023-05-22 21:28:52 -0500

    Crea componente CreateAccount

--- bank-gnb/src/components/account/CreateAccount.js
+++ bank-gnb/src/components/account/CreateAccount.js
@@ -1,9 +1,16 @@
-import { ConfigProvider } from 'antd';
-import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
-import { Breadcrumb, Layout, Menu, theme, Typography, Image, Avatar, Space} from 'antd';
+import { Layout, theme, Typography} from 'antd';
+import { Button, Form, Select,InputNumber, DatePicker} from 'antd';
 import React from 'react';
-const { Title , Text} = Typography;
-const { Header, Content, Sider} = Layout;
+const { Title } = Typography;
+const { Content } = Layout;
+
+
+const onFinish = (values) => {
+    console.log('Success:', values);
+  };
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
 
 
 const CreateAcc = () => {
@@ -22,9 +29,60 @@ const CreateAcc = () => {
         borderRadius:14,
       }}
     >
-      Hola hola estamos en crear una cuenta
+        <Form
+    name="basic"
+    labelCol={{
+      span: 8,
+    }}
+    wrapperCol={{
+      span: 16,
+    }}
+    style={{
+      maxWidth: 600,
+    }}
+    onFinish={onFinish}
+    onFinishFailed={onFinishFailed}
+    autoComplete="off"
+  >
+    <Form.Item label="Account ID" name="id" rules={[{ required: true }]}>
+        <InputNumber />
+    </Form.Item>
+
+    <Form.Item label="Account Type" name="type" rules={[{ required: true }]}>
+        <Select>
+        <Select.Option value="demo">Cuenta corriente</Select.Option>
+        <Select.Option value="demo">Cuenta de ahorros</Select.Option>
+        </Select>
+    </Form.Item>
+
+    <Form.Item label="Money" name="money" rules={[{ required: true }]}>
+        <InputNumber />
+    </Form.Item>
+
+    <Form.Item label="Date created" name="dateCreated" rules={[{ required: true }]}>
+          <DatePicker />    
+    </Form.Item>
+
+    <Form.Item label="User" name="user" rules={[{ required: true }]}>
+        <InputNumber />
+    </Form.Item>
+
+
+    <Form.Item
+      wrapperCol={{
+        offset: 8,
+        span: 16,
+      }}
+    >
+      <Button type="primary" htmlType="submit">
+        Submit
+      </Button>
+    </Form.Item>
+  </Form>
+      
     </Content>
     </div>
   )
 };
 export default CreateAcc;
+
